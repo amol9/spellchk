0b5de881dc716dd34b470f2b6f65d763c2c28113
Amol Umrale <amol@Amols-MacBook-Pro.local>
2025-05-20 18:24:55 +0530

    to be fixed: ed/ive, etc., fixed: s

--- chk.py
+++ chk.py
@@ -19,16 +19,27 @@ words = text2.split()
 
 words_in_file = text.split()
 
-print(words_in_file[0:10])
-print(len(words_in_file))
+#print(words_in_file[0:10])
+#print(len(words_in_file))
 
 for w in words_in_file:
-    print(w)
+    #print(w)
     w2 = ""
     m = re.match("[a-z]+", w)
     if m is not None:
-        w2 = m[0]
+        w2 = m[0].strip()
         
-    print(w2)
+    #print(w2)
 
-    
+    if w2 == "":
+        continue
+
+    if w2 not in words:
+        w3 = ""
+        if w2[-1] == 's':
+            w3 = w2[:-1]
+            #print(w3)
+            if w3 not in words:
+                print(w3)
+        else:
+            print(w2)
